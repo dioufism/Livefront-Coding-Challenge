1a59cf05d0423bad649e53a28148d0de80402598
Ousmane <dioufism@gmail.com>
2025-03-01 17:27:50 -0600

    Initial Commit

---
+++ LiveFrontCodingChallenge/LiveFrontCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveFrontCodingChallengeApp.swift
+//  LiveFrontCodingChallenge
+//
+//  Created by ousmane diouf on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct LiveFrontCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
