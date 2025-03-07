a987abe1092ba241324ee34fb1989e58f173862e
Ousmane <dioufism@gmail.com>
2025-03-07 00:35:55 -0600

    update secret

---
+++ LiveFrontCodingChallenge/Secret/Secrets.swift
@@ -0,0 +1,16 @@
+//
+//  Secrets.swift
+//  LiveFrontCodingChallenge
+//
+//  Created by ousmane diouf on 3/7/25.
+//
+
+import Foundation
+
+enum Secrets {
+    static let baseURL = "https://api.yelp.com/v3/"
+    
+    enum  KeyChain {
+        static let APIKey = "0Pf310N0KSmInyvZ45jFcY8D24VrXYcsVd5_Ow3Izn8-W5-y3R6Tf6R-GQUo_BZ1K1WdZEGoh09lZGmvh3EZIqag7vDiCi-lWMpYOLIMoVg1a8t5Ni9aiUkUCJzDZ3Yx"
+    }
+}
